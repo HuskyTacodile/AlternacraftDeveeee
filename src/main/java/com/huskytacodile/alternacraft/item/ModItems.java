770f8ac6c18ce931475be58550c92780bab16ea5
HuskyTacodile <huskytakidile@gmail.com>
2021-11-24 11:26:08 -0600

    fixed errors

--- src/main/java/com/huskytacodile/alternacraft/item/ModItems.java
+++ src/main/java/com/huskytacodile/alternacraft/item/ModItems.java
@@ -2,6 +2,7 @@ package com.huskytacodile.alternacraft.item;
 
 import com.huskytacodile.alternacraft.Alternacraft;
 import com.huskytacodile.alternacraft.entities.ModEntityTypes;
+import com.huskytacodile.alternacraft.item.custom.ModSpawnEggItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SpawnEggItem;
@@ -14,8 +15,8 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, Alternacraft.MOD_ID);
 
-    public static final RegistryObject<SpawnEggItem> JWGAFEMALESPINO_SPAWN_EGG = ITEMS.register("jwgafemalespino_spawn_egg",
-            () -> new SpawnEggItem(ModEntityTypes.JWGAFEMALESPINO.get(), 0x464F56, 0x1D6336,
+    public static final RegistryObject<ModSpawnEggItem> JWGAFEMALESPINO_SPAWN_EGG = ITEMS.register("jwgafemalespino_spawn_egg",
+            () -> new ModSpawnEggItem(ModEntityTypes.JWGAFEMALESPINO, 0x464F56, 0x1D6336,
                     new Item.Properties().tab(ItemGroup.TAB_MISC)));
 
     public static void register(IEventBus eventBus) {
